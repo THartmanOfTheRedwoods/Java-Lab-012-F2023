334f6d4b9776e4861d479c31597535b8a61da13e
jojov <josephverdin55@gmail.com>
2023-12-10 00:43:01 -0800

    Finished Lab 012

--- src/SolarSystem.java
+++ src/SolarSystem.java
@@ -25,8 +25,22 @@ class SolarSystem {
     }
 
     public void movePlanets() {
-        for (Planet planet : planets) {
-            planet.moveTo(planet.getXPos() + planet.getXVel(), planet.getYPos() + planet.getYVel());
+        double G = 0.1;
+        double dt = 0.001;
+
+        for(Planet p : this.planets) {
+            p.moveTo(p.getXPos() + dt * p.getXVel(),
+                    p.getYPos() + dt * p.getYVel());
+
+            double rx = this.theSun.getX() - p.getXPos();
+            double ry = this.theSun.getY() - p.getYPos();
+            double r = Math.sqrt(Math.pow(rx, 2) + Math.pow(ry, 2));
+
+            double accX = G * this.theSun.getMass() * rx / Math.pow(r, 3);
+            double accY = G * this.theSun.getMass() * ry / Math.pow(r, 3);
+
+            p.setXVel(p.getXVel() + dt * accX);
+            p.setYVel(p.getYVel() + dt * accY);
         }
     }
 }
